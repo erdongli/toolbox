5945f757b535b5e2285a3d3f228656cce480f46f
Erdong Li <1317183+erdongli@users.noreply.github.com>
2023-09-06 13:59:11 -0700

    [IP] get requester ip via x-forwarded-for

--- internal/ip/ip.go
+++ internal/ip/ip.go
@@ -1,7 +1,13 @@
 package ip
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func Handle(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(r.RemoteAddr))
+	w.Header().Add("content-type", "text/plain")
+
+	ip := strings.TrimSpace(strings.Split(r.Header.Get("x-forwarded-for"), ",")[0])
+	w.Write([]byte(ip))
 }
